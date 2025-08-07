9db150a3fc493db875ba1cadc8d71ae13e7dc13d
sayfullo077 <new81305@gmail.com>
2025-08-07 09:27:54 +0500

    added 21-lesson.py

--- lesson_20.py
+++ lesson_20.py
@@ -4,16 +4,16 @@
 # print(talaba_0)
 
 
-# son = 1  # son ga 1 qiymatini beramiz
-# ismlar = []  # Bo'sh ro'yxat hosil qilamiz
+son = 1  # son ga 1 qiymatini beramiz
+ismlar = []  # Bo'sh ro'yxat hosil qilamiz
 
-# while son <= 5:  # toki son 5 dan kichik yoki teng ekan...
-#     print(son, end=' ')  # son ni konsolga chiqaramiz
-#     ism = input("Ismingizni kiriting: ")  # foydalanuvchidan ism so'raymiz
-#     ismlar.append({"id": son, "ism": ism})  # dict shaklida ro'yxatga qo'shamiz
-#     son = son + 1  # sonni 1 ga oshiramiz
+while son <= 5:  # toki son 5 dan kichik yoki teng ekan...
+    print(son, end=' ')  # son ni konsolga chiqaramiz
+    ism = input("Ismingizni kiriting: ")  # foydalanuvchidan ism so'raymiz
+    ismlar.append({"id": son, "ism": ism})  # dict shaklida ro'yxatga qo'shamiz
+    son = son + 1  # sonni 1 ga oshiramiz
 
-# print(ismlar)  # Yakuniy natijani chiqaramiz
+print(ismlar)  # Yakuniy natijani chiqaramiz
 
 
 # print("Kiritilgan sonning kvadratini qaytaruvchi dastur.")
--- lesson_21.py
+++ lesson_21.py
@@ -1,1 +1,40 @@
-print("Hello world")
+# ism = about_me['firstname']
+# familya = about_me['lastname']
+# yosh = about_me['age']
+# yili = about_me['year']
+# ish = about_me['jobs']
+# joylashuv = about_me['address']
+
+# print(f"Mening ismim {ism} familyam {familya}, {yili}-yilda tug'ilganman, yoshim {yosh}da, {joylashuv}da {ish} bo'lib ishlayman.")
+# print(about_me.items())
+
+# for k, q in about_me.items():
+#     print(f"Kalit: {k}")
+#     print(f"Qiymat: {q}")
+
+# about_me = {'firstname': 'Sayfulloh', 'lastname': 'Mamatqulov', 'age': 28, 'year': 1997, 'jobs': 'Developer', 'address': 'Fergana'}
+
+# print(about_me.keys())
+
+
+
+bozorlik = ["banan", "non", "qaymoq", "sut", "tarvuz", "tuxum", "guruch", "go'sht"]
+mahsulotlar = {"non": 5000, "qaymoq": 50000, "tuxum": 2000, "guruch": 50000, "go'sht": 100000, "sut": 5000, "tarvuz": 3000}
+
+# for mahsulot in mahsulotlar:
+#     if mahsulot in bozorlik:
+#         print(f"{mahsulot.title()} narxi {mahsulotlar[mahsulot]} so'm")
+
+# for buyum in bozorlik:
+#     if buyum not in mahsulotlar:
+#         print(f"Iltimos, do'koningizga {buyum} ham olib keling")
+
+# print(mahsulotlar.keys())
+# print(mahsulotlar.values())
+
+# for narx in mahsulotlar.values():
+#     print(f"Mahsulot narxlari: {narx}")
+    
+davlatlar = {"O'zbekiston": "Toshkent", "Rossiya": "Moskva", "AQSH": "Vashington", "Turkiya": "Anqara", "Qozog'iston": "Nursulton"}
+for davlat, poytaxt in davlatlar.items():
+    print(f"{davlat} davlatining poytaxti {poytaxt} shahri")
